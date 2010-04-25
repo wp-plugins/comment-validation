a51da4d397596fd43cfeddf91890eb175dd2e30d
joern.zaefferer <joern.zaefferer@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-04-25 19:52:09 +0000

    fixed script includes
    git-svn-id: https://plugins.svn.wordpress.org/comment-validation/trunk@233546 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- comment-validation.php
+++ comment-validation.php
@@ -4,19 +4,16 @@ Plugin Name: Comment Validation
 Plugin URI: http://bassistance.de/wordpress-plugin-comment-validation/
 Description: Client-side validation for comments
 Author: Jörn Zaefferer
-Version: 0.3
+Version: 0.4
 Author URI: http://bassistance.de
 */
 
-function load_comment_validation_styles() {
+function load_comment_validation() {
 	wp_enqueue_style( 'commentvalidation', WP_PLUGIN_URL . '/comment-validation/comment-validation.css');
-}
-function load_comment_validation_scripts() {
 	wp_enqueue_script('jqueryvalidate', WP_PLUGIN_URL . '/comment-validation/jquery.validate.pack.js', array('jquery'));
 	wp_enqueue_script('commentvalidation', WP_PLUGIN_URL . '/comment-validation/comment-validation.js', array('jquery','jqueryvalidate'));
 }
 
-add_action('wp_print_styles', 'load_comment_validation_styles');
-add_action("wp_enqueue_scripts","load_comment_validation_scripts");
+add_action('init', 'load_comment_validation');
 
 ?>
